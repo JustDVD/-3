4d10d6e543af0b4bcd9ae6b635d7a61edc17be84
JustDVD <terteryan-d@mail.ru>
2024-10-07 04:31:28 +0300

    Add files via upload

---
+++ переменные.py
@@ -0,0 +1,5 @@
+done = 12
+hours = 1.5
+name = 'Python'
+time_for_one_homework = done / hours
+print ('Курс:', name, 'всего задач:', done, 'затрачено часов', hours, 'среднее время выполнения', time_for_one_homework)
